0fea956745d1dbc7d9c185d65d38036825f0ea58
AngelinaToste <143296606+AngelinaToste@users.noreply.github.com>
2024-05-03 18:34:16 +0000

    has default option and error handling
    uses a makeshift "try catch loop" to handle any errors in adding the user to the system

--- user_mgmt.sh
+++ user_mgmt.sh
@@ -1,46 +1,37 @@
 #!/bin/bash
 
 #create a new user
-
-
 create_user(){
     username=$1
-    password=$2
-    UID=$3
-    GID=$4
-    homedir=$5
-    shell=$6
+    password=${2:-"password"} #set a defualt password
+    homedir=${5:-"/home/$username"} #set a default homedirectory
+    shell=${6:-"/bin/bash"} #set a default shell
 
+    
+    { 
+    #try 
     echo "Input: '$username', '$password'"
     #create user with specified user name
     
-    if [ -z $password ]; then
-        $password="default"
-    elif [ -z $UID ]; then
-        $UID=1000
-    elif [ -z $GID ]; then
-        $GID=1000
-    elif [ -z $homedir ]; then
-        $homedir="/home/${$username}"
-    elif [ -z $shell ]; then
-        $shell="/bin/bas"
-    fi
-
+    
     #create user
-    sudo useradd $username
+    sudo useradd -d "$homedir" -s "$shell" "$username"
 
     #setting the password for the user
     echo "$username:$password" | sudo chpasswd
 
-    #set UID for the user
+    }||{
+    #catch
+    echo "There was an error in creating the specified user."
 
-    echo "The user '$username' has been created with the password '$password'"
+    }
+    echo "The user, '$username', has been created."
 
 }
 
+#display information for a user
 display_user(){
     username=$1
-    #password=$2
 
     echo "Here is the information for the user '$username'"
     id $username
@@ -53,6 +44,7 @@ display_user(){
     echo
 }
 
+# delete a user
 delete_user(){
     username=$1
     sudo userdel $username
@@ -61,10 +53,6 @@ delete_user(){
 
 }
 
-create_user bob password123
-
-#this function will allow management of a specified user account 
-# the command will require the username, password, and the action (create,  display, delete)
 
 if [[ $1 == "create" ]]; then 
     if [[ -n "$3" ]]; then
@@ -75,8 +63,17 @@ elif [[ $1 == "display" ]]; then
 elif [[ $1 == "delete" ]]; then 
     delete_user $2
 else
-    echo "Please specify a command (create, display, delete)"
+    echo "Please specify a command (create, display, delete) and the username. 
+Optionally, a password, home directory, and shell can be provided.
+Use the format: command username password homedirectory shell"
 fi
+
+
+
+
+
+
+
 
 
 
